03d0924d5a36bfd0638fa4bd3d65ebe1ebbe2307
Clément Rocher <clementrocher71@gmail.com>
2018-03-24 13:02:51 +0100

    Création d'activités, écran d'accueil

--- app/src/main/java/edu/bruguerolle/rocher/fanny/MainActivity.java
+++ app/src/main/java/edu/bruguerolle/rocher/fanny/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.bruguerolle.rocher.fanny;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -23,14 +25,10 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Button newMatch = (Button) findViewById(R.id.NewMatchButton);
+        Button previousMatch = (Button) findViewById(R.id.PreviousMatchesButton);
+        newMatch.setOnClickListener(newMatchHandler);
+        previousMatch.setOnClickListener(previousMatchHandler);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -98,4 +96,17 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    View.OnClickListener newMatchHandler = new View.OnClickListener() {
+        public void onClick(View v) {
+            Intent startCreateActivity = new Intent(MainActivity.this,CreateMatch.class);
+            startActivity(startCreateActivity);
+        }
+    };
+    View.OnClickListener previousMatchHandler = new View.OnClickListener() {
+        public void onClick(View v) {
+            Intent startPreviousActivity = new Intent(MainActivity.this,PreviousMatches.class);
+            startActivity(startPreviousActivity);
+        }
+    };
 }
