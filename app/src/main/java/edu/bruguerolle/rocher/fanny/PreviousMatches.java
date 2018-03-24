3d6514406080d2f0161ee0481bf8c84dcd0ca74b
Clément Rocher <clementrocher71@gmail.com>
2018-03-24 13:22:37 +0100

    Création d'activités, écran d'accueil

---
+++ app/src/main/java/edu/bruguerolle/rocher/fanny/PreviousMatches.java
@@ -0,0 +1,13 @@
+package edu.bruguerolle.rocher.fanny;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PreviousMatches extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.previous_matches);
+    }
+}
