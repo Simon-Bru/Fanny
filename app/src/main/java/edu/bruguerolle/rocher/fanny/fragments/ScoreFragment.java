1634595b2da7b7c155d5f0f0abbe73a91b741406
Simon <simon.bruguerolle@hotmail.fr>
2018-03-26 10:50:07 +0200

    Btn drawable

--- app/src/main/java/edu/bruguerolle/rocher/fanny/fragments/ScoreFragment.java
+++ app/src/main/java/edu/bruguerolle/rocher/fanny/fragments/ScoreFragment.java
@@ -1,10 +1,12 @@
 package edu.bruguerolle.rocher.fanny.fragments;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.constraint.ConstraintSet;
 import android.support.v4.app.Fragment;
+import android.text.TextWatcher;
 import android.transition.TransitionManager;
 import android.view.GestureDetector;
 import android.view.LayoutInflater;
@@ -42,6 +44,7 @@ public class ScoreFragment extends Fragment {
 
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_IS_OPPONENT = "is_opponent";
+    private static final String ARG_REMAINING_PTS = "remaining_points";
 
     private boolean isRed;
 
@@ -60,13 +63,12 @@ public class ScoreFragment extends Fragment {
      * @param isOpponent Parameter 1.
      * @return A new instance of fragment ScoreFragment.
      */
-    public static ScoreFragment newInstance(boolean isOpponent, ScoreChangeListener listener) {
+    public static ScoreFragment newInstance(boolean isOpponent) {
         ScoreFragment fragment = new ScoreFragment();
         Bundle args = new Bundle();
 
         args.putBoolean(ARG_IS_OPPONENT, isOpponent);
         fragment.setArguments(args);
-        fragment.mListener = listener;
         return fragment;
     }
 
@@ -106,6 +108,42 @@ public class ScoreFragment extends Fragment {
     }
 
     @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (savedInstanceState != null) {
+            isRed = savedInstanceState.getBoolean(ARG_IS_OPPONENT);
+            remainingPoints = pointsImg.length - 1;
+
+            while(remainingPoints > savedInstanceState.getInt(ARG_REMAINING_PTS)) {
+                movePoint(true);
+            }
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle state) {
+        super.onSaveInstanceState(state);
+        state.putBoolean(ARG_IS_OPPONENT, isRed);
+        state.putInt(ARG_REMAINING_PTS, remainingPoints);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof ScoreChangeListener) {
+            mListener = (ScoreChangeListener)context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement ScoreChangeListener");
+        }
+    }
+
+    @Override
     public void onDetach() {
         super.onDetach();
         mListener = null;
@@ -122,10 +160,59 @@ public class ScoreFragment extends Fragment {
      * >Communicating with Other Fragments</a> for more information.
      */
     public interface ScoreChangeListener {
-        void onPointMarked(boolean isPositive);
-        void onMatchOver();
+        void onPointMarked(boolean isRed, boolean isPositive);
+        void onMatchOver(boolean isRed);
     }
 
+    private void movePoint(boolean isPosivitive) {
+        ConstraintLayout container = root.findViewById(R.id.pointsContainer);
+        TransitionManager.beginDelayedTransition(container);
+
+        ImageView point;
+        ConstraintSet constraintSet = new ConstraintSet();
+        constraintSet.clone(container);
+
+        if(isPosivitive && remainingPoints >= 0) {
+            point = root.findViewById(pointsImg[remainingPoints]);
+            constraintSet = pullRight(constraintSet, point.getId());
+
+            if(remainingPoints == 0) {
+                mListener.onMatchOver(isRed);
+            } else {
+                remainingPoints =  remainingPoints - 1;
+            }
+        } else if(remainingPoints < pointsImg.length-1){
+            remainingPoints++;
+
+            point = root.findViewById(pointsImg[remainingPoints]);
+            constraintSet = pullLeft(constraintSet, point.getId());
+        }
+
+        constraintSet.applyTo(container);
+    }
+
+    private ConstraintSet pullRight(ConstraintSet constraintSet, int pointId) {
+        if(remainingPoints < pointsImg.length - 1) {
+            constraintSet.connect(pointId, ConstraintSet.END,
+                    pointsImg[remainingPoints+1], ConstraintSet.END,35);
+        } else {
+            constraintSet.connect(pointId, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END,0);
+        }
+        constraintSet.clear(pointId, ConstraintSet.START);
+
+        return constraintSet;
+    }
+
+    private ConstraintSet pullLeft(ConstraintSet constraintSet, int pointId) {
+        constraintSet.connect(pointId, ConstraintSet.START,
+                (remainingPoints > 0 ?
+                        pointsImg[remainingPoints - 1] :
+                        ConstraintLayout.LayoutParams.PARENT_ID),
+                ConstraintSet.START,  remainingPoints > 0 ? 30 : 0);
+        constraintSet.clear(pointId, ConstraintSet.END);
+
+        return constraintSet;
+    }
 
     /**
      * Class for slide listening on the points bar
@@ -141,62 +228,16 @@ public class ScoreFragment extends Fragment {
         @Override
         public boolean onFling(MotionEvent event1, MotionEvent event2,
                                float velocityX, float velocityY) {
+//            IF HORIZONTAL SCROLL
             if(Math.abs(velocityX) > Math.abs(velocityY) &&
                     remainingPoints < pointsImg.length && remainingPoints >= 0) {
-
-                ConstraintLayout container = root.findViewById(R.id.pointsContainer);
-                TransitionManager.beginDelayedTransition(container);
-
-                ImageView point;
-                ConstraintSet constraintSet = new ConstraintSet();
-                constraintSet.clone(container);
-
-                if(velocityX > 0 && remainingPoints >= 0) {
-                    point = getActivity().findViewById(pointsImg[remainingPoints]);
-                    constraintSet = pullRight(constraintSet, point);
-
-                    if(remainingPoints == 0) {
-                        mListener.onMatchOver();
-                    } else {
-                        mListener.onPointMarked(true);
-                        remainingPoints =  remainingPoints - 1;
-                    }
-                } else if(remainingPoints < pointsImg.length-1){
-                    remainingPoints++;
-                    mListener.onPointMarked(false);
-
-                    point = getActivity().findViewById(pointsImg[remainingPoints]);
-                    constraintSet = pullLeft(constraintSet, point);
-                }
-
-                constraintSet.applyTo(container);
+                boolean isPosivitive = velocityX > 0;
+                mListener.onPointMarked(isRed, isPosivitive);
+                movePoint(isPosivitive);
                 return true;
             } else {
                 return false;
             }
-        }
-
-        private ConstraintSet pullRight(ConstraintSet constraintSet, ImageView point) {
-            if(remainingPoints < pointsImg.length - 1) {
-                constraintSet.connect(point.getId(), ConstraintSet.END,
-                        pointsImg[remainingPoints+1], ConstraintSet.END,45);
-            } else {
-                constraintSet.connect(point.getId(), ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END,0);
-            }
-            constraintSet.clear(point.getId(), ConstraintSet.START);
-
-            return constraintSet;
-        }
-
-        private ConstraintSet pullLeft(ConstraintSet constraintSet, ImageView point) {
-            constraintSet.connect(point.getId(), ConstraintSet.START,
-                    (remainingPoints > 0 ?
-                            pointsImg[remainingPoints - 1] :
-                            ConstraintLayout.LayoutParams.PARENT_ID),
-                    ConstraintSet.START,  remainingPoints > 0 ? 45 : 0);
-            constraintSet.clear(point.getId(), ConstraintSet.END);
-
-            return constraintSet;
         }
     }
 }
